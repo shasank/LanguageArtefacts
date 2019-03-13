4bdd68d48061555119166ef2b06320b16b18ed66
shasank <shasank_me@yahoo.com>
2019-03-13 23:41:38 +0530

    Delete shasank.d.ts

--- lib/dts/shasank.d.ts
+++
@@ -1,9 +0,0 @@
-declare const shasank: shasank;
-interface shasank {
-	getClientData(key: string) : string
-	getFullName() : string
-	hasRole(role: string) : boolean
-	hasRoleExactly(role: string) : boolean
-	hasRoleFromList(roles: string) : boolean
-    hasRoles() : boolean
-}
