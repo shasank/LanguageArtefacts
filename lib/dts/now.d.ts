c21dab7e148abe1be5ee578dbdd38df611022a47
shasank <shasank_me@yahoo.com>
2019-03-13 23:41:28 +0530

    Delete now.d.ts

--- lib/dts/now.d.ts
+++
@@ -1,9 +0,0 @@
-declare const now: now;
-interface now {
-	getClientData(key: string) : string
-	getFullName() : string
-	hasRole(role: string) : boolean
-	hasRoleExactly(role: string) : boolean
-	hasRoleFromList(roles: string) : boolean
-    hasRoles() : boolean
-}
